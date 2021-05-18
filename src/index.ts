b7404701338e225afd8dffefeb7cd9b8273b49f3
Chan Kang <me@kentakang.com>
2021-05-18 18:37:32 +0900

    feat: add test file

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import core from '@actions/core';
+import http, { RequestOptions, ServerResponse } from 'http';
+
+try {
+  const youtubeId = core.getInput('account-id');
+  const youtubePw = core.getInput('account-password');
+  const requestOptions: RequestOptions = {
+    hostname: 'music.youtube.com',
+    path: '/',
+  };
+
+  http.request(requestOptions, (response) => {
+    let serverData = '';
+
+    response.on('data', (chunk) => {
+      serverData += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(serverData);
+    });
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
