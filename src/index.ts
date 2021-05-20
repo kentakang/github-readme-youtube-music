12093f64eae1b96d8de195c0ab8bf986d7371f93
Chan Kang <me@kentakang.com>
2021-05-20 14:30:30 +0900

    fix: fix async logic

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,27 @@ import { getInput, setFailed, setOutput } from '@actions/core';
 import { exec } from 'child_process';
 
 console.log('installing puppeteer');
-exec('sudo npm i puppeteer --unsafe-perm=true --allow-root', async () => {
+exec('sudo npm i puppeteer --unsafe-perm=true --allow-root', () => {
   try {
     const puppeteer = require('puppeteer');
     const youtubeId = getInput('account-id');
     const youtubePw = getInput('account-password');
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
 
-    await page.goto('https://music.youtube.com', { waitUntil: 'networkidle0' });
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-    const data = await page.content();
+      await page.goto('https://music.youtube.com', { waitUntil: 'networkidle0' });
 
-    console.log('data', data);
-    setOutput('data', data);
+      const data = await page.content();
+
+      console.log('data', data);
+      setOutput('data', data);
+
+      await browser.close();
+
+      return new Promise((resolve) => resolve('end'));
+    })().then((r) => console.log(r), (v) => console.log(v));
   } catch (error) {
     setFailed(error.message);
   }
