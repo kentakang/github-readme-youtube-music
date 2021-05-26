d74e37cba24c3f1d28682c63a485343d75c7ccf3
Chan Kang <me@kentakang.com>
2021-05-26 16:11:53 +0900

    fix: change bundler ncc to webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  externals: nodeExternals(),
+};
